663c5210b768703f4d7e7e50176ba1ddf116c7ab
Olga  Fedina <ofedin1@students.towson.edu>
2020-10-27 20:45:34 -1000

    first bit of fractal code

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -1,62 +1,40 @@
-
-var minval = -0.5;
-var maxval = 0.5;
-
-var minSlider;
-var maxSlider;
-
-var frDiv;
-
-function setup() {
-  createCanvas(200, 200);
+function setup(){
+  createCanvas(360, 360);
   pixelDensity(1);
-
-  minSlider = createSlider(-2.5, 0, -2.5, 0.01);
-  maxSlider = createSlider(0, 2.5, 2.5, 0.01);
-
-  frDiv = createDiv('');
-}
-
-function draw() {
-  var maxiterations = 100;
-
   loadPixels();
-  for (var x = 0; x < width; x++) {
-    for (var y = 0; y < height; y++) {
-      var a = map(x, 0, width, minSlider.value(), maxSlider.value());
-      var b = map(y, 0, height, minSlider.value(), maxSlider.value());
+  for(var x = 0; x < width; x++){
+    for(var y = 0; y < height; y++){
 
-      var ca = a;
-      var cb = b;
+      var a = map(x, 0, width, -2, 2);
+      var b = map(y, 0, height, -2, 2);
 
       var n = 0;
+      var z = 0;
 
-      while (n < maxiterations) {
-        var aa = a * a - b * b;
-        var bb = 2 * a * b;
-        a = aa + ca;
-        b = bb + cb;
-        if (a * a + b * b > 16) {
-          break;
-        }
-        n++;
-      }
+      while(n<100){
+          var aa = a*a - b*b;
+          var bb = 2*a*b;
 
-      var bright = map(n, 0, maxiterations, 0, 1);
-      bright = map(sqrt(bright), 0, 1, 0, 255);
+          a = aa;
+          b = bb;
 
-      if (n == maxiterations) {
-        bright = 0;
+          if( abs(a + b) > 16){
+            break;
+          }
+
+          n++;
       }
 
       var pix = (x + y * width) * 4;
-      pixels[pix + 0] = bright;
-      pixels[pix + 1] = bright;
-      pixels[pix + 2] = bright;
-      pixels[pix + 3] = 255;
+      pixels[pix + 0] = 51;
+      pixels[pix + 1] = 51;
+      pixels[pix + 2] = 51;
+      pixels[pix + 3] = 51;
     }
   }
-  updatePixels();
 
-  frDiv.html(floor(frameRate()));
+}
+
+function draw(){
+
 }
